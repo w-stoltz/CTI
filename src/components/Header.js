81711a950f9efa4994f0a8845411d49f21ad7db3
wstoltz <wstoltz@email.arizona.edu>
2018-05-15 23:16:13 -0700

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+import logo from '../assets/images/icons8-airplane-take-off-filled-100.png';
+
+const Header = (props) => (
+    <header id="header" className="alt">
+        <span className="logo"><img src={logo} alt="" /></span>
+        <h1>CTI: Transporation</h1>
+        <p>Cyber Threat Intelligence Platform Proposal<br />
+        William Stoltz | Marc Padilla</p>
+    </header>
+)
+
+export default Header
